5749c959a1a37db6b4349108978dc2a42f7c6af9
Kyle Stewart <kylestew@gmail.com>
2014-10-25 15:33:30 -0700

    Podified

--- HotOrGram/AppDelegate.h
+++ HotOrGram/AppDelegate.h
@@ -1,17 +1,8 @@
-//
-//  AppDelegate.h
-//  HotOrGram
-//
-//  Created by Kyle Stewart on 10/25/14.
-//  Copyright (c) 2014 Kyle Stewart. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
-
 
 @end
 
---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, '8.0'
+
+source 'https://github.com/CocoaPods/Specs.git'
+
+pod 'SVProgressHUD'
+pod "AFNetworking", "~> 2.0"
+pod 'ReactiveCocoa'
+pod 'libextobjc'
+pod 'Mantle'
+
